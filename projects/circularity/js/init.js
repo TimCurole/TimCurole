755af23eaf48ad523e33fc87f6f932d361234332
TimCurole <curole530@stcharles.k12.la.us>
2023-04-20 17:59:22 +0000

    refractor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,14 +20,21 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle(){
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+         }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
-
+         for (var completed = 0; completed < 100; completed++){
+            drawCircle()
+         }
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -39,14 +46,15 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
-            
+            for (var i = 0; i < circles.length; i++){
+                physikz.updatePosition(circles[i])
+                game.checkCirclePosition(circles[i])
+            }
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
-
-            // TODO 9 : Iterate over the array
-           
             
+            
+            // TODO 9 : Iterate over the array
+    
         }
     
         /* 
@@ -62,7 +70,15 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if (circle.x < 0){
+                circle.x = canvas.width
+            }
+            if (circle.y > 700){
+                circle.y = 0
+            }
+            if (circle.y < 0){
+                circle.y = 700
+            }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
